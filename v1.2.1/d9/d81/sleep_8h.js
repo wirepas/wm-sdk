574cf465e1f8b46132142be134e65726dd4d9a1a
GwendalRaoul <GwendalRaoul@users.noreply.github.com>
2021-06-04 14:39:41 +0000

    deploy: dac5f64201851aa04bbf514a738bd4d62ee05b75

---
+++ v1.2.1/d9/d81/sleep_8h.js
@@ -0,0 +1,24 @@
+var sleep_8h =
+[
+    [ "app_lib_sleep_t", "d9/d81/sleep_8h.html#d1/d14/structapp__lib__sleep__t", [
+      [ "getSleepLatestGotosleep", "d9/d81/sleep_8h.html#a00e7656bcc56cd6d880ec3013e9d8fdb", null ],
+      [ "getSleepState", "d9/d81/sleep_8h.html#a8f0e63d0f7449498de50ccf489ac60ff", null ],
+      [ "getStackWakeup", "d9/d81/sleep_8h.html#ab6b7f0018cd76a0543762654f42112e8", null ],
+      [ "setOnWakeupCb", "d9/d81/sleep_8h.html#ac64cb93973d4b86b989af1e0baf97b8e", null ],
+      [ "sleepStackforTime", "d9/d81/sleep_8h.html#ad33684ef1960c8c4c5cff08823be02b3", null ],
+      [ "wakeupStack", "d9/d81/sleep_8h.html#a25ddf26fb90e38fc6c591bf1e6585e0a", null ]
+    ] ],
+    [ "app_lib_sleep_get_gotosleep", "d9/d81/sleep_8h.html#a072055652d7cd4b58ba28a73ddb49528", null ],
+    [ "app_lib_sleep_get_sleep_state_f", "d9/d81/sleep_8h.html#a48fa5da91eca7255d1c10aa0aa711de6", null ],
+    [ "app_lib_sleep_get_wakeup_f", "d9/d81/sleep_8h.html#a125a4f5ebe7eff4538ec46a96ab2630d", null ],
+    [ "app_lib_sleep_stack_for_time_f", "d9/d81/sleep_8h.html#afb1ec08432833f278c37ca72797dc9ac", null ],
+    [ "app_lib_sleep_wakeup_callback_f", "d9/d81/sleep_8h.html#a5ca8115c9730523953ae51abf03fa50b", null ],
+    [ "app_lib_sleep_wakeup_stack_f", "d9/d81/sleep_8h.html#ac129d28de628e04dea54cd1c2a0c615d", null ],
+    [ "applib_wakeup_callback_f", "d9/d81/sleep_8h.html#aec8777d66c5b7f53e6b67341f8b8e49d", null ],
+    [ "app_lib_sleep_stack_state_e", "d9/d81/sleep_8h.html#a3dd4ee1781c33c2e0dceeab6becf3d00", [
+      [ "APP_LIB_SLEEP_STOPPED", "d9/d81/sleep_8h.html#a3dd4ee1781c33c2e0dceeab6becf3d00a388cc689f1f108470e04b9b2e0910cec", null ],
+      [ "APP_LIB_SLEEP_STARTED", "d9/d81/sleep_8h.html#a3dd4ee1781c33c2e0dceeab6becf3d00af38e9ec1d42fe61c56df03c7ed232fe7", null ]
+    ] ],
+    [ "APP_LIB_LONGSLEEP_NAME", "d9/d81/sleep_8h.html#abd628831f264eacccded967c1495b89a", null ],
+    [ "APP_LIB_LONGSLEEP_VERSION", "d9/d81/sleep_8h.html#af9197873a67e15027c9e8794eae87ab2", null ]
+];
