62d95550d3c900a32af6cf6fc74a6a3a80479f1f
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2021-11-15 16:27:09 -0800

    add custom board files; update build for dualmcu_app

--- config.mk
+++ config.mk
@@ -1,12 +1,12 @@
 # Name of your app. It is used to name the produced files
 # It has to be the same as the app directory name under source
 # It can be overwritten on command line "make app_name=<...>"
-app_name=custom_app
+app_name=dualmcu_app
 
 # Name of the board to build your application
 # If build is always with same board, it can be set here
 # It can be overwritten on command line "make target_board=<...>"
-target_board=
+target_board=nrf52840_bmd345
 
 # Version of the SDK
 sdk_major=1
--- source/reference_apps/dualmcu_app/config.mk
+++ source/reference_apps/dualmcu_app/config.mk
@@ -1,5 +1,5 @@
 # Boards compatible with this app 
-TARGET_BOARDS := mdbt50q_rx nrf52832_mdk_v2 pca10040 pca10056 pca10059 pca10100 promistel_rpi_hat ublox_b204 wuerth_261101102 silabs_brd4254a tbsense2 silabs_brd4253a silabs_brd4180b silabs_brd4181b silabs_brd4184a 
+TARGET_BOARDS := mdbt50q_rx nrf52832_mdk_v2 pca10040 pca10056 pca10059 pca10100 promistel_rpi_hat ublox_b204 wuerth_261101102 silabs_brd4254a tbsense2 silabs_brd4253a silabs_brd4180b silabs_brd4181b silabs_brd4184a nrf52840_bmd345
 
 # Define a specific application area_id
 app_specific_area_id=0x846B74
